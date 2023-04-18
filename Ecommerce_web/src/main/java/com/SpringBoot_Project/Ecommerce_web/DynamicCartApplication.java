aaf147bf31590a1b61db93c8a2c4ddeb38a75198
shubhneet <shubhneet.aggrwal@tothenew.com>
2023-04-18 13:56:37 +0530

    Initialize Project

---
+++ Ecommerce_web/src/main/java/com/SpringBoot_Project/Ecommerce_web/DynamicCartApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBoot_Project.Ecommerce_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicCartApplication.class, args);
+	}
+
+}
---
+++ Ecommerce_web/src/test/java/com/SpringBoot_Project/Ecommerce_web/DynamicCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoot_Project.Ecommerce_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
